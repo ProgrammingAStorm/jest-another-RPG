e69af2cc95e35b8501b6fc1bffc8e8b62d03381f
Mark Pavel <markpavel02@gmail.com>
2022-06-01 15:23:00 -0400

    Added Enemy Class And Enemy Tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,8 @@
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion')
 
+jest.mock('../lib/Potion');
+
 test('creates a player object', () => {
     const player = new Player('Dave');
   
